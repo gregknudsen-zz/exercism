438b2b3f904975a1812cbe8ef53512881d4102c9
Greg Knudsen <knudsegi@hotmail.com>
2013-09-09 21:08:11 -0400

    Added Bob with 13 passing tests

--- ruby/bob/bob.rb
+++ ruby/bob/bob.rb
@@ -3,23 +3,29 @@ require_relative 'bob_test'
 class Bob
 	
 	def hey(string)
-		if all_caps(string)
+		if string_is_empty?(string)
+			"Fine. Be that way!"		
+		elsif all_caps(string)
 			"Woah, chill out!" 
+		elsif string_ends_in_a_question_mark?(string)
+			"Sure."
 		else
 			"Whatever."
 		end
 	end
-	
-	# def all_caps(string)
-	# 	if string == string.upcase
-	# 		true
-	# 	else
-	# 		false
-	# 	end
-	# end
 
+	def string_is_empty?(string)
+		string.empty?
+	end
+
+
+	#change to 'all_caps?'
 	def all_caps(string)
 		string == string.upcase
+	end
+
+	def string_ends_in_a_question_mark?(string)
+		string.end_with?("?")
 	end
 
 
